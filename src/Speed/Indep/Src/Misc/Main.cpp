a62765f17ea91f17784c3088e1a83edaafaa923d
CreateSource <72283721+abnormalhare@users.noreply.github.com>
2025-06-03 21:03:19 -0400

    start main.cpp

--- src/Speed/Indep/Src/Misc/Main.cpp
+++ src/Speed/Indep/Src/Misc/Main.cpp
@@ -1,3 +1,30 @@
+// OUTSIDE ZMISC
+class RaceStarter {
+public:
+    static void StartSkipFERace();
+};
+
+// IN ZMISC
+extern void InitializeEverything(int argc, char *argv[]);
+extern void WriteFreekerBaseAddressBeacon();
+extern int SkipFE;
+class GameFlowManager {
+public:
+    void LoadFrontend();
+};
+extern struct GameFlowManager TheGameFlowManager;
+
 int main(int argc, char *argv[]) {
-    
+    const float minimum_time_step = 0.0f;
+    unsigned int current_tick;
+    float milliseconds;
+    int milliseconds_fix;
+
+    InitializeEverything(argc, argv);
+    WriteFreekerBaseAddressBeacon();
+    if (SkipFE != 0) {
+        RaceStarter::StartSkipFERace();
+    } else {
+        TheGameFlowManager.LoadFrontend();
+    }
 }
