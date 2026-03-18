79dc985e187f71d89a3a5c965a7a756eb843d6af
Johann Berger <johann@objekt.stream>
2026-03-18 10:43:43 +0100

    63.150%: rebuild GObjectBlock instance scanning

--- src/Speed/Indep/Src/Gameplay/GObjectBlock.cpp
+++ src/Speed/Indep/Src/Gameplay/GObjectBlock.cpp
@@ -1,8 +1,64 @@
 #include "Speed/Indep/Src/Gameplay/GObjectBlock.h"
 
+#include "Speed/Indep/Libs/Support/Utility/FastMem.h"
+#include "Speed/Indep/Src/Gameplay/GActivity.h"
+#include "Speed/Indep/Src/Gameplay/GCharacter.h"
+#include "Speed/Indep/Src/Gameplay/GHandler.h"
+#include "Speed/Indep/Src/Gameplay/GManager.h"
+#include "Speed/Indep/Src/Gameplay/GMarker.h"
+#include "Speed/Indep/Src/Gameplay/GState.h"
+#include "Speed/Indep/Src/Gameplay/GTrigger.h"
 #include "Speed/Indep/Src/Gameplay/GVault.h"
 #include "Speed/Indep/Tools/AttribSys/Runtime/AttribSys.h"
 #include "Speed/Indep/bWare/Inc/Strings.hpp"
+
+template <typename T>
+static unsigned int GetPaddedObjectSize() {
+    return (sizeof(T) + 15) & ~15;
+}
+
+template <typename T>
+static unsigned int FindInstances(GVault *vault, AttribKeyList *attribKeyList, unsigned int *outObjCount, unsigned int *outConnCount) {
+    static unsigned int kObjectTemplateKey = 0x7FCB7ABA;
+    int numConnections = 0;
+    Attrib::Gen::gameplay objectTemplate(Attrib::FindCollection(Attrib::Gen::gameplay::ClassKey(), kObjectTemplateKey), 0, nullptr);
+    int numObjects = 0;
+    Attrib::Vault *attribVault = vault->GetAttribVault();
+    unsigned int exportIndex = 0;
+    unsigned int exportCount;
+    int collectionType = Attrib::StringToTypeID("Attrib::CollectionLoadData");
+
+    exportCount = attribVault->CountExports();
+    if (exportCount != 0) {
+        do {
+            if (attribVault->GetExportType(exportIndex) == collectionType) {
+                unsigned int collectionKey = Attrib::GetCollectionKey(reinterpret_cast<Attrib::Collection *>(attribVault->GetExportData(exportIndex)));
+                Attrib::Gen::gameplay instanceObj(Attrib::FindCollection(Attrib::Gen::gameplay::ClassKey(), collectionKey), 0, nullptr);
+
+                if (GObjectBlock::CollectionIsInstanceOfTemplate(instanceObj, objectTemplate)) {
+                    if (attribKeyList) {
+                        attribKeyList->push_back(collectionKey);
+                    }
+
+                    numObjects = numObjects + 1;
+                    numConnections = numConnections + GObjectBlock::CalcNumConnections(collectionKey);
+                }
+            }
+
+            exportIndex = exportIndex + 1;
+        } while (exportIndex < exportCount);
+    }
+
+    if (outObjCount) {
+        *outObjCount = *outObjCount + numObjects;
+    }
+
+    if (outConnCount) {
+        *outConnCount = *outConnCount + numConnections;
+    }
+
+    return (numObjects * GetPaddedObjectSize<T>() + numConnections * 8 + 0xFU) & ~0xFU;
+}
 
 GObjectBlock::GObjectBlock(GVault *vault, unsigned char *buffer)
     : mVault(vault), //
@@ -13,14 +69,45 @@ GObjectBlock::~GObjectBlock() {}
 void GObjectBlock::Initialize(unsigned int bufferSize) {}
 
 unsigned int GObjectBlock::CalcSpaceRequired(GVault *vault, unsigned int *outObjCount) {
+    unsigned int objCount = 0;
+    unsigned int spaceRequired = FindInstances<GHandler>(vault, nullptr, &objCount, nullptr);
+
+    spaceRequired += FindInstances<GState>(vault, nullptr, &objCount, nullptr);
+    spaceRequired += FindInstances<GActivity>(vault, nullptr, &objCount, nullptr);
+    spaceRequired += FindInstances<GCharacter>(vault, nullptr, &objCount, nullptr);
+    spaceRequired += FindInstances<GMarker>(vault, nullptr, &objCount, nullptr);
+    spaceRequired += FindInstances<GTrigger>(vault, nullptr, &objCount, nullptr);
+
     if (outObjCount) {
-        *outObjCount = 0;
+        *outObjCount = objCount;
     }
 
-    return 0;
+    return spaceRequired;
 }
 
 bool GObjectBlock::CollectionIsInstanceOfTemplate(Attrib::Gen::gameplay &instanceObj, Attrib::Gen::gameplay &templateObj) {
+    const int *isObject = reinterpret_cast<const int *>(instanceObj.GetAttributePointer(0x3E9156CA, 0));
+    unsigned int parentKey;
+
+    if (!isObject) {
+        isObject = reinterpret_cast<const int *>(Attrib::DefaultDataArea(sizeof(int)));
+    }
+
+    if (*isObject != 0) {
+        return false;
+    }
+
+    parentKey = instanceObj.GetParent();
+    while (parentKey != 0) {
+        Attrib::Gen::gameplay parent(Attrib::FindCollection(Attrib::Gen::gameplay::ClassKey(), parentKey), 0, nullptr);
+
+        if (parent.GetCollection() == templateObj.GetCollection()) {
+            return true;
+        }
+
+        parentKey = parent.GetParent();
+    }
+
     return false;
 }
 
