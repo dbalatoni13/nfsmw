d745154a2126aaf828c45ed6092b77786560fbb1
Johann Berger <johann@objekt.stream>
2026-03-18 12:39:51 +0100

    66.14%: implement GHandler filter checks

--- src/Speed/Indep/Src/Gameplay/GHandler.cpp
+++ src/Speed/Indep/Src/Gameplay/GHandler.cpp
@@ -4,9 +4,24 @@
 #include "Speed/Indep/Src/Lua/source/lauxlib.h"
 #include "Speed/Indep/Src/Lua/source/lua.h"
 #include "Speed/Indep/Src/Lua/LuaRuntime.h"
+#include "Speed/Indep/Src/Misc/Hermes.h"
 #include "Speed/Indep/Src/Misc/LZCompress.hpp"
 
 void LZByteSwapHeader(LZHeader *header);
+
+typedef void (*QueryFunc)(IMessageFilterContext *, int, const void *, int, bool *);
+
+struct MessageFilterHeader {
+    unsigned char mSourceOffset;
+    unsigned char mDestOffset;
+    unsigned char mSize;
+    unsigned char mInitialized;
+    QueryFunc mQuery;
+    unsigned char mQueryStaticData[1];
+};
+
+void ByteSwapStaticData(const void *data) __asm__("ByteSwapStaticData__5QueryPCv");
+QueryFunc LookupQueryFunc(unsigned int key) __asm__("LookupQueryFunc__5QueryUi");
 
 GHandler::GHandler(const Attrib::Key &handlerKey)
     : GRuntimeInstance(handlerKey, kGameplayObjType_Handler), //
@@ -69,6 +84,53 @@ void GHandler::Detach(lua_State *luaState) {
 
 void GHandler::HandleMessage(LuaMessageDeliveryInfo *deliveryInfo) {
     ExecuteScriptedHandler(deliveryInfo);
+}
+
+bool GHandler::MessagePassesFilters(LuaMessageDeliveryInfo *deliveryInfo) {
+    struct BlobView {
+        unsigned int mSize;
+        const void *mData;
+    };
+
+    for (unsigned int onFilter = 0;; onFilter++) {
+        if (onFilter >= Num_FilterBlocks()) {
+            return true;
+        }
+
+        const Attrib::Blob *blobPtr = reinterpret_cast<const Attrib::Blob *>(GetAttributePointer(0x56e1436d, onFilter));
+
+        if (blobPtr) {
+            Attrib::Blob blob = *blobPtr;
+            const BlobView &blobView = reinterpret_cast<const BlobView &>(blob);
+            unsigned char *blockData = reinterpret_cast<unsigned char *>(const_cast<void *>(blobView.mData));
+            MessageFilterHeader *filter = reinterpret_cast<MessageFilterHeader *>(blockData);
+            bool filterPassed = true;
+
+            if (!filter->mInitialized) {
+                ByteSwapStaticData(filter->mQueryStaticData);
+                filter->mQuery = LookupQueryFunc(*reinterpret_cast<unsigned int *>(filter->mQueryStaticData));
+                filter->mInitialized = 1;
+            }
+
+            IMessageFilterContext *filterContext = deliveryInfo;
+            const unsigned char *source = reinterpret_cast<const unsigned char *>(filterContext->GetMessage()) + sizeof(Hermes::Message) + filter->mSourceOffset;
+            unsigned char *dest = blockData + filter->mDestOffset + 0xC;
+
+            bMemCpy(dest, source, filter->mSize);
+
+            if (filter->mQuery) {
+                filter->mQuery(filterContext, 0, filter->mQueryStaticData, 1, &filterPassed);
+            }
+
+            if (!FilterModePassAll(0)) {
+                if (filterPassed) {
+                    return true;
+                }
+            } else if (!filterPassed) {
+                return false;
+            }
+        }
+    }
 }
 
 void GHandler::ExecuteScriptedHandler(LuaMessageDeliveryInfo *deliveryInfo) {
