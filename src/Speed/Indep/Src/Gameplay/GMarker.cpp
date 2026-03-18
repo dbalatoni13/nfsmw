842de99ad550fa6947ac3155c8fdcb11b50fba30
Johann Berger <johann@objekt.stream>
2026-03-18 12:37:59 +0100

    65.90%: rewrite GMarker constructor

--- src/Speed/Indep/Src/Gameplay/GMarker.cpp
+++ src/Speed/Indep/Src/Gameplay/GMarker.cpp
@@ -4,18 +4,26 @@
 
 GMarker::GMarker(const Attrib::Key &markerKey)
     : GRuntimeInstance(markerKey, kGameplayObjType_Marker) {
-    Attrib::Gen::gameplay gameplayObj(markerKey, 0, nullptr);
     UMath::Matrix4 rotMat;
-    UMath::Vector3 position;
-    UMath::Vector3 forward = {0.0f, 0.0f, 1.0f};
+    UMath::Vector3 initialVec = UMath::Vector3Make(0.0f, 0.0f, 1.0f);
+    const UMath::Vector3 *posPtr = reinterpret_cast<const UMath::Vector3 *>(GetAttributePointer(0x9f743a0e, 0));
+    const float *rotation = reinterpret_cast<const float *>(GetAttributePointer(0x5a6a57c6, 0));
 
-    position = gameplayObj.Position(0);
-    UMath::MultYRot(UMath::Matrix4::kIdentity, -gameplayObj.Rotation(0) * 0.00069444446f, rotMat);
-    VU0_MATRIX3x4_vect3mult(forward, rotMat, mDirection);
+    if (!posPtr) {
+        posPtr = reinterpret_cast<const UMath::Vector3 *>(Attrib::DefaultDataArea(sizeof(UMath::Vector3)));
+    }
 
-    mPosition.x = -position.y;
-    mPosition.y = position.z;
-    mPosition.z = position.x;
+    if (!rotation) {
+        rotation = reinterpret_cast<const float *>(Attrib::DefaultDataArea(sizeof(float)));
+    }
+
+    UMath::MultYRot(UMath::Matrix4::kIdentity, -*rotation * 0.00069444446f, rotMat);
+    VU0_MATRIX3x4_vect3mult(initialVec, rotMat, initialVec);
+
+    mPosition.x = -posPtr->y;
+    mPosition.y = posPtr->z;
+    mPosition.z = posPtr->x;
+    mDirection = initialVec;
 }
 
 GMarker::~GMarker() {}
