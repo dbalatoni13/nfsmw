eaf81c81cdccd42d7db8e546291477ed82dce78b
Johann Berger <johann@objekt.stream>
2026-03-19 22:16:08 +0100

    78.2%: improve GMilestone::NotifyPursuitOver

--- src/Speed/Indep/Src/Gameplay/GMilestone.cpp
+++ src/Speed/Indep/Src/Gameplay/GMilestone.cpp
@@ -118,25 +118,21 @@ void GMilestone::NotifyPursuitOver(bool escaped) {
             }
 
             {
+                Attrib::Gen::gameplay gameplayObj(mChallengeKey, 0, nullptr);
                 GRaceBin *bin = GRaceDatabase::Get().GetBinNumber(mBinNumber);
+                const int *bounty = reinterpret_cast<const int *>(gameplayObj.GetAttributePointer(0x8E1904C7, 0));
 
                 if (bin) {
                     bin->RefreshProgress();
                 }
-            }
-
-            {
-                Attrib::Gen::gameplay gameplayObj(mChallengeKey, 0, nullptr);
-                const int *bounty = reinterpret_cast<const int *>(gameplayObj.GetAttributePointer(0x8E1904C7, 0));
 
                 if (!bounty) {
                     bounty = reinterpret_cast<const int *>(Attrib::DefaultDataArea(sizeof(int)));
                 }
 
                 Game_AwardPlayerBounty(*bounty);
+                Game_ChallengeCompleted();
             }
-
-            Game_ChallengeCompleted();
         } else {
             mState = kState_Available;
         }
