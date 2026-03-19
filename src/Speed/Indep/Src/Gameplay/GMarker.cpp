cff852b7748223d448e7f766ebf870398e9eaa1c
Johann Berger <johann@objekt.stream>
2026-03-19 22:20:58 +0100

    78.3%: improve GMarker::GMarker

--- src/Speed/Indep/Src/Gameplay/GMarker.cpp
+++ src/Speed/Indep/Src/Gameplay/GMarker.cpp
@@ -6,13 +6,18 @@ GMarker::GMarker(const Attrib::Key &markerKey)
     : GRuntimeInstance(markerKey, kGameplayObjType_Marker) {
     const UMath::Vector3 *pos = reinterpret_cast<const UMath::Vector3 *>(GetAttributePointer(0x9F743A0E, 0));
     UMath::Matrix4 rotMat = UMath::Matrix4::kIdentity;
-    UMath::Vector3 initialVec = UMath::Vector3Make(0.0f, 0.0f, 1.0f);
-    const float *rotation = reinterpret_cast<const float *>(GetAttributePointer(0x5A6A57C6, 0));
+    UMath::Vector3 initialVec;
+    const float *rotation;
+
+    initialVec.x = 0.0f;
+    initialVec.y = 0.0f;
+    initialVec.z = 1.0f;
 
     if (!pos) {
         pos = reinterpret_cast<const UMath::Vector3 *>(Attrib::DefaultDataArea(sizeof(UMath::Vector3)));
     }
 
+    rotation = reinterpret_cast<const float *>(GetAttributePointer(0x5A6A57C6, 0));
     if (!rotation) {
         rotation = reinterpret_cast<const float *>(Attrib::DefaultDataArea(sizeof(float)));
     }
@@ -21,9 +26,11 @@ GMarker::GMarker(const Attrib::Key &markerKey)
     VU0_MATRIX3x4_vect3mult(initialVec, rotMat, initialVec);
 
     mPosition.x = -pos->y;
-    mPosition.y = pos->z;
     mPosition.z = pos->x;
-    mDirection = initialVec;
+    mPosition.y = pos->z;
+    mDirection.x = initialVec.x;
+    mDirection.z = initialVec.z;
+    mDirection.y = initialVec.y;
 }
 
 GMarker::~GMarker() {}
