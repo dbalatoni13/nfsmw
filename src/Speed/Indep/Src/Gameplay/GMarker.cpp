4d51ec7b43ca7005647b2062249050a2e22de116
Johann Berger <johann@objekt.stream>
2026-03-19 23:11:52 +0100

    78.7%: improve GMarker::GMarker

--- src/Speed/Indep/Src/Gameplay/GMarker.cpp
+++ src/Speed/Indep/Src/Gameplay/GMarker.cpp
@@ -4,33 +4,15 @@
 
 GMarker::GMarker(const Attrib::Key &markerKey)
     : GRuntimeInstance(markerKey, kGameplayObjType_Marker) {
-    const UMath::Vector3 *pos = reinterpret_cast<const UMath::Vector3 *>(GetAttributePointer(0x9F743A0E, 0));
+    const UMath::Vector3 &pos = Position(0);
     UMath::Matrix4 rotMat = UMath::Matrix4::kIdentity;
-    UMath::Vector3 initialVec;
-    const float *rotation;
+    UMath::Vector3 initialVec = UMath::Vector3Make(0.0f, 0.0f, 1.0f);
 
-    initialVec.x = 0.0f;
-    initialVec.y = 0.0f;
-    initialVec.z = 1.0f;
+    UMath::MultYRot(rotMat, -Rotation(0) * 0.0027777778f, rotMat);
+    UMath::Rotate(initialVec, rotMat, initialVec);
 
-    if (!pos) {
-        pos = reinterpret_cast<const UMath::Vector3 *>(Attrib::DefaultDataArea(sizeof(UMath::Vector3)));
-    }
-
-    rotation = reinterpret_cast<const float *>(GetAttributePointer(0x5A6A57C6, 0));
-    if (!rotation) {
-        rotation = reinterpret_cast<const float *>(Attrib::DefaultDataArea(sizeof(float)));
-    }
-
-    MATRIX4_multyrot(&rotMat, -*rotation * 0.0027777778f, &rotMat);
-    VU0_MATRIX3x4_vect3mult(initialVec, rotMat, initialVec);
-
-    mPosition.x = -pos->y;
-    mPosition.z = pos->x;
-    mPosition.y = pos->z;
-    mDirection.x = initialVec.x;
-    mDirection.z = initialVec.z;
-    mDirection.y = initialVec.y;
+    mPosition = UMath::Vector3Make(-pos.y, pos.z, pos.x);
+    mDirection = initialVec;
 }
 
 GMarker::~GMarker() {}
