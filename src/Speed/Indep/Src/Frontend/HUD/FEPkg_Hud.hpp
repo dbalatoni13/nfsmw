bb0afc8d08cba925cba5b78ec7c50ecd2e847962
Noxis <61048207+NoxieNya@users.noreply.github.com>
2026-04-05 00:33:24 +0000

    fix missing HudFeaturesType typedef

--- src/Speed/Indep/Src/Frontend/HUD/FEPkg_Hud.hpp
+++ src/Speed/Indep/Src/Frontend/HUD/FEPkg_Hud.hpp
@@ -27,43 +27,43 @@ class FEngHud : public UTL::COM::Object, public IHud {
     FEngHud(ePlayerHudType ht, const char *pkg_name, IPlayer *player, int player_number);
 
   private:
-    unsigned long long CurrentHudFeatures; // offset 0x20, size 0x8
-    ePlayerHudType mPlayerHudType;         // offset 0x28, size 0x4
-    const char *pPackageName;              // offset 0x2C, size 0x4
-    IPlayer *pPlayer;                      // offset 0x30, size 0x4
-    int PlayerNumber;                      // offset 0x34, size 0x4
-    ActionQueue mActionQ;                  // offset 0x38, size 0x294
-    bool mInPursuit;                       // offset 0x2CC, size 0x1
-    bool mHasTurbo;                        // offset 0x2D0, size 0x1
-    HudElement *pSpeedometer;              // offset 0x2D4, size 0x4
-    HudElement *pTachometer;               // offset 0x2D8, size 0x4
-    HudElement *pTachometerDrag;           // offset 0x2DC, size 0x4
-    HudElement *pShiftUpdater;             // offset 0x2E0, size 0x4
-    HudElement *pCostToState;              // offset 0x2E4, size 0x4
-    HudElement *pReputation;               // offset 0x2E8, size 0x4
-    HudElement *pHeatMeter;                // offset 0x2EC, size 0x4
-    HudElement *pTurboMeter;               // offset 0x2F0, size 0x4
-    HudElement *pEngineTemp;               // offset 0x2F4, size 0x4
-    HudElement *pNitrous;                  // offset 0x2F8, size 0x4
-    HudElement *pSpeedBreakerMeter;        // offset 0x2FC, size 0x4
-    HudElement *pRaceOverMessage;          // offset 0x300, size 0x4
-    HudElement *pGenericMessage;           // offset 0x304, size 0x4
-    HudElement *pAutoSaveIcon;             // offset 0x308, size 0x4
-    HudElement *pRaceInformation;          // offset 0x30C, size 0x4
-    HudElement *pLeaderBoard;              // offset 0x310, size 0x4
-    HudElement *pPursuitBoard;             // offset 0x314, size 0x4
-    HudElement *pMilestoneBoard;           // offset 0x318, size 0x4
-    HudElement *pBustedMeter;              // offset 0x31C, size 0x4
-    HudElement *pTimeExtension;            // offset 0x320, size 0x4
-    HudElement *pWrongWIndi;               // offset 0x324, size 0x4
-    HudElement *pOnlineSupport;            // offset 0x328, size 0x4
-    HudElement *p321Go;                    // offset 0x32C, size 0x4
-    HudElement *pRadarDetector;            // offset 0x330, size 0x4
-    HudElement *pMinimap;                  // offset 0x334, size 0x4
-    HudElement *pGetAwayMeter;             // offset 0x338, size 0x4
-    HudElement *pMenuZoneTrigger;          // offset 0x33C, size 0x4
-    HudElement *pInfractions;              // offset 0x340, size 0x4
-    bool mCurrentWidescreenSetting;        // offset 0x344, size 0x1
+    HudFeaturesType CurrentHudFeatures; // offset 0x20, size 0x8
+    ePlayerHudType mPlayerHudType;      // offset 0x28, size 0x4
+    const char *pPackageName;           // offset 0x2C, size 0x4
+    IPlayer *pPlayer;                   // offset 0x30, size 0x4
+    int PlayerNumber;                   // offset 0x34, size 0x4
+    ActionQueue mActionQ;               // offset 0x38, size 0x294
+    bool mInPursuit;                    // offset 0x2CC, size 0x1
+    bool mHasTurbo;                     // offset 0x2D0, size 0x1
+    HudElement *pSpeedometer;           // offset 0x2D4, size 0x4
+    HudElement *pTachometer;            // offset 0x2D8, size 0x4
+    HudElement *pTachometerDrag;        // offset 0x2DC, size 0x4
+    HudElement *pShiftUpdater;          // offset 0x2E0, size 0x4
+    HudElement *pCostToState;           // offset 0x2E4, size 0x4
+    HudElement *pReputation;            // offset 0x2E8, size 0x4
+    HudElement *pHeatMeter;             // offset 0x2EC, size 0x4
+    HudElement *pTurboMeter;            // offset 0x2F0, size 0x4
+    HudElement *pEngineTemp;            // offset 0x2F4, size 0x4
+    HudElement *pNitrous;               // offset 0x2F8, size 0x4
+    HudElement *pSpeedBreakerMeter;     // offset 0x2FC, size 0x4
+    HudElement *pRaceOverMessage;       // offset 0x300, size 0x4
+    HudElement *pGenericMessage;        // offset 0x304, size 0x4
+    HudElement *pAutoSaveIcon;          // offset 0x308, size 0x4
+    HudElement *pRaceInformation;       // offset 0x30C, size 0x4
+    HudElement *pLeaderBoard;           // offset 0x310, size 0x4
+    HudElement *pPursuitBoard;          // offset 0x314, size 0x4
+    HudElement *pMilestoneBoard;        // offset 0x318, size 0x4
+    HudElement *pBustedMeter;           // offset 0x31C, size 0x4
+    HudElement *pTimeExtension;         // offset 0x320, size 0x4
+    HudElement *pWrongWIndi;            // offset 0x324, size 0x4
+    HudElement *pOnlineSupport;         // offset 0x328, size 0x4
+    HudElement *p321Go;                 // offset 0x32C, size 0x4
+    HudElement *pRadarDetector;         // offset 0x330, size 0x4
+    HudElement *pMinimap;               // offset 0x334, size 0x4
+    HudElement *pGetAwayMeter;          // offset 0x338, size 0x4
+    HudElement *pMenuZoneTrigger;       // offset 0x33C, size 0x4
+    HudElement *pInfractions;           // offset 0x340, size 0x4
+    bool mCurrentWidescreenSetting;     // offset 0x344, size 0x1
 };
 
 // total size: 0xC
