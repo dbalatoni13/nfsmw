7ecfdbb6e7d71d78205a8541981adc8f145646bd
dbalatoni13 <40299962+dbalatoni13@users.noreply.github.com>
2026-06-30 19:08:02 +0200

    94.4%: reconstruct RoadblockFlow SyncRoadblock

--- src/Speed/Indep/Src/Speech/RoadblockFlow.cpp
+++ src/Speed/Indep/Src/Speech/RoadblockFlow.cpp
@@ -1,8 +1,13 @@
 #include "RoadblockFlow.h"
 #include "SoundAI.h"
+#include "Speed/Indep/Libs/Support/Utility/UMath.h"
 #include "Speed/Indep/Src/EAXSound/Stream/SpeechManager.hpp"
+#include "Speed/Indep/Src/Generated/Messages/MMiscSound.h"
 #include "Speed/Indep/Src/Generated/Messages/MNotifySpeechStatus.h"
 #include "Speed/Indep/Src/Generated/Messages/MReqRoadBlock.h"
+#include "Speed/Indep/Src/Interfaces/Simables/IAI.h"
+#include "Speed/Indep/Src/Interfaces/Simables/IRenderable.h"
+#include "Speed/Indep/Src/Interfaces/SimEntities/IPlayer.h"
 
 namespace Speech {
 
@@ -63,28 +68,87 @@ void RoadblockFlow::MessageRoadBlockDodged(const MReqRoadBlock &) {
 }
 
 void RoadblockFlow::SyncRoadblock() {
-    if ((mFlags & REQUESTED) == 0) {
+    SoundAI *ai = UTL::Collections::Singleton<SoundAI>::Get();
+    if (ai == 0) {
+        return;
+    }
+
+    UMath::Vector3 pPos = UMath::Vector3::kZero;
+    IPlayer *player = IPlayer::First(PLAYER_LOCAL);
+    IRoadBlock *roadblock = ai->GetRoadblock();
+    unsigned int flags = mFlags;
+
+    if ((flags & RESET_PENDING) != 0) {
+        float reset_time = (WorldTimer - mT_reset).GetSeconds();
+        if (mPertinentRB != roadblock && reset_time >= ai->GetTune().RBPostOutcomeResetTime()) {
+            mPertinentRB = roadblock;
+            mFlags = flags & ~RESET_PENDING;
+            if ((flags & SETUP) != 0) {
+                mFlags |= REQ_SERVICE;
+            }
+            mFlags &= ~(SETUP | LOS | HELIJOINED | AVERTED | NAILED_SPIKES | NAILED_OTHEROBJ | NAILED_VEHICLE | ENGAGED | POSITIONED);
+            ++mNumBlocks;
+        }
+    }
+
+    if (roadblock != 0 && player != 0 && (mFlags & RESET_PENDING) == 0) {
+        roadblock->GetPursuit();
+        pPos = player->GetPosition();
+        {
+            UMath::Vector3 rbToPlayer;
+            UMath::Sub(ai->GetRoadblock()->GetRoadBlockCentre(), pPos, rbToPlayer);
+            mLoDist2RB = UMath::Length(rbToPlayer);
+        }
+
+        flags = mFlags;
+        if (((flags ^ SETUP) & SETUP) != 0) {
+            flags |= SETUP;
+            mFlags = flags | REQ_SERVICE;
+            mT_setup = WorldTimer;
+        }
+
+        bool has_los = false;
+        const IRoadBlock::Vehicles &vehicles = roadblock->GetVehicles();
+        if (vehicles.size() != 0) {
+            for (IRoadBlock::Vehicles::const_iterator i = vehicles.begin(); i != vehicles.end(); ++i) {
+                IVehicle *car = *i;
+                IPursuitAI *car_ai;
+                IRenderable *renderable;
+                if (car->QueryInterface(&car_ai)) {
+                    car->QueryInterface(&renderable);
+                    if (car_ai->GetTimeSinceTargetSeen() <= 0.0f || (renderable != 0 && renderable->InView())) {
+                        has_los = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        if (has_los) {
+            if ((mFlags & LOS) == 0) {
+                flags = mFlags | LOS;
+                mFlags = flags | REQ_SERVICE;
+                MMiscSound message(1);
+                message.Send(UCrc32("RBLOS"));
+            }
+        } else {
+            mFlags &= ~LOS;
+        }
+    }
+
+    flags = mFlags;
+    if ((flags & RB_ENABLED) == 0 && ai->RoadblocksEnabled()) {
         Request();
-        return;
-    }
-
-    if ((mFlags & SETUP) == 0) {
-        Setup();
-        return;
-    }
-
-    if ((mFlags & POSITIONED) == 0) {
-        Approach();
-        return;
-    }
-
-    if ((mFlags & ENGAGED) == 0) {
-        Effect();
-        return;
-    }
-
-    if ((mFlags & REQ_SERVICE) != 0) {
-        Service();
+    } else if ((flags & HELIRB_ENABLED) == 0 && ai->HeliRoadblocksEnabled()) {
+        Request();
+    } else if ((flags & SPIKES_ENABLED) == 0 && ai->SpikesEnabled()) {
+        Request();
+    } else if ((flags & RB_ENABLED) != 0 && !ai->RoadblocksEnabled()) {
+        Request();
+    } else if ((flags & HELIRB_ENABLED) != 0 && !ai->HeliRoadblocksEnabled()) {
+        Request();
+    } else if ((flags & SPIKES_ENABLED) != 0 && !ai->SpikesEnabled()) {
+        Request();
     }
 }
 
