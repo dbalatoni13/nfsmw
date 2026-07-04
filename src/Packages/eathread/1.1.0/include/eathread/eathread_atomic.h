00c724daa354af4ea9baa0eb1d8d32eed3187554
CreateSource <abnormalhare822@gmail.com>
2026-07-04 18:34:57 -0400

    and these

---
+++ src/Packages/eathread/1.1.0/include/eathread/eathread_atomic.h
@@ -0,0 +1,3 @@
+#ifdef EA_PLATFORM_GAMECUBE
+#include "eathread/powerpc/eathread_atomic_powerpc.h"
+#endif
---
+++ src/Packages/eathread/1.1.0/include/eathread/powerpc/eathread_semaphore_powerpc.h
@@ -0,0 +1,69 @@
+#ifndef EATHREAD_EATHREAD_SEMAPHORE_H
+#define EATHREAD_EATHREAD_SEMAPHORE_H
+
+#include "dolphin/os/OSSemaphore.h"
+#include "eathread/eathread_atomic.h"
+
+struct EASemaphoreData {
+    OSSemaphore mSemaphore; // offset 0x0, size 0xC
+    AtomicInt<int> mnCount; // offset 0xC, size 0x4
+
+    EASemaphoreData();
+};
+
+namespace EA {
+namespace Thread {
+
+struct SemaphoreParameters {
+    SemaphoreParameters(int initialCount, bool, const char *);
+
+    // Members
+    int mInitialCount; // offset 0x0, size 0x4
+    bool mbIntraProcess; // offset 0x4, size 0x1
+    char mName[16]; // offset 0x8, size 0x10
+};
+
+typedef unsigned int ThreadTime;
+
+class Semaphore {
+public:
+    enum Result {
+        kResultError = -1,
+        kResultTimeout = -2,
+    };
+
+protected:
+    EASemaphoreData mSemaphoreData; // offset 0x0, size 0x10
+
+public:
+    Semaphore() {}
+
+    ~Semaphore();
+
+    bool Init(const SemaphoreParameters *pSemaphoreParameters) ;
+
+    int Wait(const ThreadTime &timeoutAbsolute) ;
+
+    int Post(int count) ;
+
+    int GetCount() const;
+
+    void * GetPlatformData() {}
+
+private:
+    // Semaphore &operator=() {}
+
+public:
+    Semaphore(const struct SemaphoreParameters * pSemaphoreParameters, bool bDefaultParameters);
+
+private:
+    Semaphore(int initialCount);
+};
+
+void ThreadSleep(const ThreadTime &timeout);
+unsigned int GetThreadTime();
+
+}
+}
+
+#endif
