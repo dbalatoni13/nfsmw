28c95d152f2675dd0732546ccf702a469e82758e
dbalatoni13 <40299962+dbalatoni13@users.noreply.github.com>
2026-07-18 19:21:41 +0200

    22.4%: implement LobbyGames update and lock controls

--- src/Speed/Indep/Src/Online/LobbyGames.cpp
+++ src/Speed/Indep/Src/Online/LobbyGames.cpp
@@ -37,3 +37,65 @@ int32 LobbyGames::StartGame(CommandCBFunc startGameCB, void *context, bool kickU
     lobbyMutex.Unlock("LobbyGames::StartGame");
     return -1;
 }
+
+int32 LobbyGames::UpdateGame(CommandCBFunc updateGameCB, void *context) {
+    lobbyMutex.Lock("LobbyGames::UpdateGame");
+    if (myCurrentGame.iIdent != -1 && bStrCmp(myCurrentGame.strHost, FEDatabase->OnlineSettings.GetLobbyPersona()) == 0) {
+        char buf[64] = "";
+        TagFieldSetString(buf, sizeof(buf), "NAME", myCurrentGame.strName);
+        TagFieldSetNumber(buf, sizeof(buf), "MINSIZE", FEDatabase->OnlineSettings.MinOnlinePlayers);
+        int32 rc = LobbyCore::Instance().QueueCommand('gset', buf, LobbyCore::DefaultCB, nullptr, updateGameCB, context, false);
+        lobbyMutex.Unlock("LobbyGames::UpdateGame");
+        return rc;
+    }
+    lobbyMutex.Unlock("LobbyGames::UpdateGame");
+    return -1;
+}
+
+inline int32 LobbyGames::LockGame(CommandCBFunc lockGameCB, void *context) {
+    lobbyMutex.Lock("LobbyGames::LockGame");
+    if (myCurrentGame.iIdent != -1 && bStrCmp(myCurrentGame.strHost, FEDatabase->OnlineSettings.GetLobbyPersona()) == 0 &&
+        !(myCurrentGame.uSysflags & 0x1000)) {
+        bool areSomePlayersReady = false;
+        for (int i = 0; i < myCurrentGame.iCount; i++) {
+            if (myCurrentGame.aOpponents[i].uFlags & 0x08000000) {
+                areSomePlayersReady = true;
+            }
+        }
+        char buf[128] = "";
+        TagFieldSetString(buf, sizeof(buf), "NAME", myCurrentGame.strName);
+        TagFieldSetNumber(buf, sizeof(buf), "SYSFLAGS", myCurrentGame.uSysflags | 0x1000);
+        TagFieldSetNumber(buf, sizeof(buf), "USERFLAGS",
+                          areSomePlayersReady ? myCurrentGame.aOpponents[0].uFlags | 0x08000000
+                                              : myCurrentGame.aOpponents[0].uFlags);
+        int32 rc = LobbyCore::Instance().QueueCommand('gset', buf, LobbyCore::DefaultCB, nullptr, lockGameCB, context, false);
+        lobbyMutex.Unlock("LobbyGames::LockGame");
+        return rc;
+    }
+    lobbyMutex.Unlock("LobbyGames::LockGame");
+    return -1;
+}
+
+int32 LobbyGames::UnlockGame(CommandCBFunc unlockGameCB, void *context) {
+    lobbyMutex.Lock("LobbyGames::UnlockGame");
+    if (myCurrentGame.iIdent != -1 && bStrCmp(myCurrentGame.strHost, FEDatabase->OnlineSettings.GetLobbyPersona()) == 0 &&
+        (myCurrentGame.uSysflags & 0x1000)) {
+        char buf[128] = "";
+        TagFieldSetString(buf, sizeof(buf), "NAME", myCurrentGame.strName);
+        TagFieldSetNumber(buf, sizeof(buf), "SYSFLAGS", myCurrentGame.uSysflags & ~0x1000);
+        TagFieldSetNumber(buf, sizeof(buf), "USERFLAGS", myCurrentGame.aOpponents[0].uFlags & ~0x08000000);
+        int32 rc = LobbyCore::Instance().QueueCommand('gset', buf, LobbyCore::DefaultCB, nullptr, unlockGameCB, context, false);
+        lobbyMutex.Unlock("LobbyGames::UnlockGame");
+        return rc;
+    }
+    lobbyMutex.Unlock("LobbyGames::UnlockGame");
+    return -1;
+}
+
+LobbyApiPlayT *LobbyGames::GetMyGame() const {
+    LobbyApiPlayT *game = nullptr;
+    if (myCurrentGame.iIdent != -1) {
+        game = const_cast<LobbyApiPlayT *>(&myCurrentGame);
+    }
+    return game;
+}
