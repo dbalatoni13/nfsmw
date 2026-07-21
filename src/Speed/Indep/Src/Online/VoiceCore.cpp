7f18572be8db5da6dce0948b1539508a8035d9d8
dbalatoni13 <40299962+dbalatoni13@users.noreply.github.com>
2026-07-21 21:01:18 +0200

    38.4%: match PushToTalk StartPolling

--- src/Speed/Indep/Src/Online/VoiceCore.cpp
+++ src/Speed/Indep/Src/Online/VoiceCore.cpp
@@ -1,3 +1,14 @@
 #include "VoiceCore.hpp"
 
 VoiceCore::PushToTalk::PushToTalk() { mIsPolling = false; }
+
+void VoiceCore::PushToTalk::StartPolling() {
+    if (!mIsPolling) {
+        for (int i = 0; i < 2; i++) {
+            mIsPressed[i] = false;
+            mActionQ[i] = new ActionQueue(i, 0x6689019e, "VOIP", false);
+            mActionQ[i]->Enable(true);
+        }
+        mIsPolling = true;
+    }
+}
