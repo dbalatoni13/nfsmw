71c7ef52307655d32d2297a0b1fe482046fca827
dbalatoni13 <40299962+dbalatoni13@users.noreply.github.com>
2026-07-21 22:30:52 +0200

    48.7%: match CWebOffer lifecycle methods

--- src/Speed/Indep/Src/Online/WebOfferUG2.cpp
+++ src/Speed/Indep/Src/Online/WebOfferUG2.cpp
@@ -1,8 +1,23 @@
 #include "WebOfferUG2.hpp"
 
+extern "C" WebOfferT *WebOfferCreate(uint32 maxScriptSize);
+extern "C" void WebOfferDestroy(WebOfferT *webOffer);
+
 CWebOffer::CWebOffer()
     : m_pWebOfferAPI(nullptr) //
     , m_pCurrentCommand(nullptr) //
     , m_bProcessingCommand(false) {}
 
 CWebOffer::~CWebOffer() { Shutdown(); }
+
+bool CWebOffer::Initialise(uint32 MaxScriptSize) {
+    m_pWebOfferAPI = WebOfferCreate(MaxScriptSize);
+    return m_pWebOfferAPI != nullptr;
+}
+
+void CWebOffer::Shutdown() {
+    if (m_pWebOfferAPI) {
+        WebOfferDestroy(m_pWebOfferAPI);
+        m_pWebOfferAPI = nullptr;
+    }
+}
