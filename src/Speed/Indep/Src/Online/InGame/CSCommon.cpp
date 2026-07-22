10c9a44c999a530003cd55afa2780a92a826af26
dbalatoni13 <40299962+dbalatoni13@users.noreply.github.com>
2026-07-22 02:30:31 +0200

    72.2%: implement CSCommon ShowDiagnostics

--- src/Speed/Indep/Src/Online/InGame/CSCommon.cpp
+++ src/Speed/Indep/Src/Online/InGame/CSCommon.cpp
@@ -1,4 +1,11 @@
 #include "CSCommon.hpp"
+#include "Speed/Indep/Src/Misc/Timer.hpp"
+
+extern int DoScreenPrintf;
+extern int ShowMiscOnlineDiagnostics;
+extern volatile float PreviousCpuFrameRate;
+extern volatile float PreviousGpuFrameRate;
+void ScreenPrintf(int x, int y, const char *format, ...);
 
 void CSCommon::DumpBytes(const char *raw_data, int num_bytes) {
     for (int i = 0; i < num_bytes; i++) {
@@ -6,3 +13,15 @@ void CSCommon::DumpBytes(const char *raw_data, int num_bytes) {
 }
 
 void CSCommon::CarOverlayDiagnostics() {}
+
+void CSCommon::ShowDiagnostics() {
+    if (DoScreenPrintf && ShowMiscOnlineDiagnostics) {
+        ScreenPrintf(-100, 120, "WorldTimeSeconds: %f", WorldTimeSeconds);
+        int fps = static_cast<int>((PreviousCpuFrameRate < PreviousGpuFrameRate
+                                        ? PreviousCpuFrameRate
+                                        : PreviousGpuFrameRate) +
+                                   0.5f);
+        ScreenPrintf(280, -235, "%d", fps);
+    }
+    CarOverlayDiagnostics();
+}
