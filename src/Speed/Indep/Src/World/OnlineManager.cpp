26e91ebae568db35cba63d38e072c4cec3dd6ba8
dbalatoni13 <40299962+dbalatoni13@users.noreply.github.com>
2026-08-04 01:11:23 +0200

    63.5%: match OnlineManager quantizer initialization

--- src/Speed/Indep/Src/World/OnlineManager.cpp
+++ src/Speed/Indep/Src/World/OnlineManager.cpp
@@ -1,3 +1,28 @@
 #include "OnlineManager.hpp"
 
 OnlineManager TheOnlineManager;
+
+void OnlineManager::InitQuantizers() {
+    float min_value;
+    float max_value;
+
+    PositionQuantizerX.Init("PositionQuantizerX", -4800.0f, 1792.0f, 0.01f);
+    PositionQuantizerY.Init("PositionQuantizerY", -15.0f, 300.0f, 0.01f);
+    max_value = 1.0f;
+    PositionQuantizerZ.Init("PositionQuantizerZ", -1664.0f, 4224.0f, 0.01f);
+    min_value = 0.0f;
+    VelocityQuantizer.Init("VelocityQuantizer", -125.0f, 125.0f, 0.1f);
+    MatrixQuantizer.Init("MatrixQuantizer", -1.0f, max_value, 0.01f);
+    AngleQuantizer.Init("AngleQuantizer", -0.5f, 0.5f, 0.0025f);
+    AccelerationQuantizer.Init("AccelerationQuantizer", -25.0f, 25.0f, 0.1f);
+    AVelocityQuantizer.Init("AVelocityQuantizer", -5.0f, 5.0f, 0.01f);
+    ControlQuantizer.Init("ControlQuantizer", min_value, max_value, 0.15f);
+    QuantFloatSimTime.Init("QuantFloatSimTime", min_value, 5400.0f, 0.001f);
+    QuantCarSlotID.Init("QuantCarSlotID", 0, 0x8b);
+    QuantCarPartIndex.Init("QuantPartIndex", 0, 16000);
+    QuantCarType.Init("QuantCarType", 0, 0x4e);
+    QuantInt2Bit.Init("QuantInt2Bit", 0, 3);
+    QuantInt3Bit.Init("QuantInt3Bit", 0, 7);
+    QuantInt4Bit.Init("QuantInt4Bit", 0, 0xf);
+    QuantFloatTime.Init("QuantFloatTime", min_value, 14400.0f, 0x18);
+}
