a1463c94a446e22d8c8dccc78c398eb6ab7d82fe
dbalatoni13 <40299962+dbalatoni13@users.noreply.github.com>
2026-08-04 04:58:36 +0200

    68.4%: improve OnlineManager StartRace match

--- src/Speed/Indep/Src/World/OnlineManager.cpp
+++ src/Speed/Indep/Src/World/OnlineManager.cpp
@@ -807,20 +807,12 @@ void OnlineManager::StartRace() {
     TheAnimPlayer.UnPauseAll();
     RaceEndReason = OEND_RACE_FINISHED;
 
-    OnlineRacer **racer = pRacers;
     int driver_number = 0;
+    OnlineRacer **racer = pRacers;
     do {
         if (*racer) {
             OnlineRacer *online_racer = GetOnlineRacer(driver_number);
-            eOnlineRacerState state = online_racer->State;
-            bool connected = false;
-            if (state != OPS_LOST_CONNECTION && state != OPS_QUIT &&
-                state != OPS_DISCONNECTED) {
-                connected = true;
-                if (state == OPS_DISCERROR) {
-                    connected = false;
-                }
-            }
+            bool connected = online_racer->IsConnected();
             if (connected) {
                 GetOnlineRacer(driver_number)->ChangeState(OPS_RACING);
             }
@@ -831,5 +823,5 @@ void OnlineManager::StartRace() {
 
     mPostCountdownStartRaceTime = 0.0f;
     mStartTime = mMasterTime;
-    ChangeState(OLS_RACING);
+    return ChangeState(OLS_RACING);
 }
