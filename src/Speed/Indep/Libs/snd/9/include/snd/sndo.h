6f1bcf759533ccdda6991375d15ca078f42f5f3d
CreateSource <abnormalhare822@gmail.com>
2026-08-13 19:59:53 -0400

    match sbvalid, scheckpo, sclnt100, sctrldry, sdata, sfxlevel, slowpass, smemdis, smemlmt, smemcpy matches but doesnt link

--- src/Speed/Indep/Libs/snd/9/include/snd/sndo.h
+++ src/Speed/Indep/Libs/snd/9/include/snd/sndo.h
@@ -300,8 +300,6 @@ int SNDSTRM_destroy(int sndstreamhandle);
 void SNDSYS_entercritical();
 void SNDSYS_leavecritical();
 void SNDSYS_service();
-void SNDSYS_remove100hzclient(void (*client)());
-void SNDSYS_add100hzclient(void (*client)());
 
 int SNDtimeremaining(int shandle);
 
