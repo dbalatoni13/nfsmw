472b8f22d6dc92f780a229fe80c99e99fc11646f
dbalatoni13 <40299962+dbalatoni13@users.noreply.github.com>
2026-08-18 20:34:26 +0200

    Match A124 screen effect update

--- src/Speed/Indep/Src/World/ScreenEffects.cpp
+++ src/Speed/Indep/Src/World/ScreenEffects.cpp
@@ -138,6 +138,9 @@ void UpdateAllScreenEFX() {
                 debugflash = 0;
                 eGetView(i, false)->ScreenEffects->AddPaletteEffect(EFX_CAMERA_FLASH);
             }
+#ifdef EA_BUILD_A124
+            eGetView(i, false)->ScreenEffects->AddPaletteEffect(EFX_UNIQUE);
+#endif
         }
     }
 }
