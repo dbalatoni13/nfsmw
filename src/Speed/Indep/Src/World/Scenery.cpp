20f4d115e015f2028e38cec2b64c0346c00fd41b
dbalatoni13 <40299962+dbalatoni13@users.noreply.github.com>
2026-08-18 03:08:18 +0200

    Match A124 scenery view selection

--- src/Speed/Indep/Src/World/Scenery.cpp
+++ src/Speed/Indep/Src/World/Scenery.cpp
@@ -914,13 +914,14 @@ int GrandSceneryCullInfo::WhatSectionsShouldWeDraw(short *sections_to_draw, int 
     int iViewID = scenery_cull_info->pView->GetID();
 #ifdef EA_BUILD_A124
     if (iViewID == 17 || iViewID == 18) {
-        int iViewPlayer = iViewID - 16;
+        drivable_scenery_section = TheVisibleSectionManager.FindDrivableSection(
+            reinterpret_cast<const bVector2 *>(eGetView(1, false)->GetCamera()->GetPosition()));
 #else
     if (iViewID == EVIEW_SHADOWMAP1 || iViewID == EVIEW_SHADOWMAP2) {
         int iViewPlayer = iViewID - 12;
-#endif
         drivable_scenery_section = TheVisibleSectionManager.FindDrivableSection(
             reinterpret_cast<const bVector2 *>(eGetView(iViewPlayer, false)->GetCamera()->GetPosition()));
+#endif
     } else {
         drivable_scenery_section =
             TheVisibleSectionManager.FindDrivableSection(reinterpret_cast<const bVector2 *>(scenery_cull_info->pView->GetCamera()->GetPosition()));
